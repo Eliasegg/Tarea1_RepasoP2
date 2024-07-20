8825c5b9a485d0c112354681feb79858526f4ddb
Eliasegg <elias-eeg@hotmail.com>
2024-07-20 11:03:05 -0600

    Metodo de Recursividad: MCD

--- src/repasop2_eliasenamorado/Recursividad.java
+++ src/repasop2_eliasenamorado/Recursividad.java
@@ -14,5 +14,16 @@ public class Recursividad {
         }
         return decimalABinario(numero / 2) + (numero % 2);
     }
+    
+    
+    // Idea principal es ir dividiendo el número mayor entre el menor y quedarse con el resto (Algoritmo de Euclides)
+    // El caso base es cuando el segundo número es 0
+    public static int mcd(int num1, int num2) {
+        if (num2 == 0) {
+            return num1;
+        }
+        return mcd(num2, num1 % num2);
+    }
+    
 
 }
